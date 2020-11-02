3b71b7ce2a181bdd33ee912485bd68d4e4b5e623
Yvain16-69 <72392571+Yvain16-69@users.noreply.github.com>
2020-11-02 16:05:51 +0100

    02/11

---
+++ js/pong.js
@@ -0,0 +1,13 @@
+alert("coucou")
+
+let largeur=$("balle").width();
+let gauche=parseInt($("balle").cssleft("left"));
+let haut=parseInt($("balle").cssleft("left"));
+alert(gauche);
+
+setInterval(function()(
+    gauche-gauche;
+    haut-haut;
+    $("#balle").css("left",gauche);
+    $("balle").css("top"),haut);
+), 10);
