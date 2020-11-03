02445a9f7d35f2357874d1a06270909189f54633
Yvain Di Costanzo <72392571+Yvain16-69@users.noreply.github.com>
2020-11-03 19:42:11 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -2,12 +2,12 @@
 
 let largeur=$("balle").width();
 let gauche=parseInt($("balle").cssleft("left"));
-let haut=parseInt($("balle").cssleft("left"));
+let haut=parseInt($("balle").csstop("top"));
 alert(gauche);
 
-setInterval(function()(
-    gauche-gauche;
-    haut-haut;
+setInterval(function(){
+    gauche=gauche+1;
+    haut=haut+0.5;
     $("#balle").css("left",gauche);
-    $("balle").css("top"),haut);
-), 10);
+    $("#balle").css("top",haut);
+}, 10);
