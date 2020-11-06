117accc7f20cf78cd8b579a196fdf27d6848b8e5
Yvain Di Costanzo <72392571+Yvain16-69@users.noreply.github.com>
2020-11-06 19:23:41 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -3,10 +3,13 @@
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
+//Message d'alerte à l'ouverture de la page indiquant la position de la balle
 alert(gauche);
 
 setInterval(function(){
+    //Donne un mouvement à la balle vers la droite
     gauche=gauche+1;
+    //Donne un mouvement à la balle vers le bas
     haut=haut+0.5;
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
