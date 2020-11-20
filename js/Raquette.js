f3585f89e0c13379f780748898ef9df47c954b1e
Yvain Di Costanzo <72392571+Yvain16-69@users.noreply.github.com>
2020-11-20 14:25:46 +0100

    Update Raquette.js

--- js/Raquette.js
+++ js/Raquette.js
@@ -1,32 +1,71 @@
-class Raquette1{
+class Raquette{ //dans les classes, on ne fait que déclarer des variables, pour faire des actions, il faut utiliser une fonction
     constructor($html){
         this.$html=$html;
-        this.haut=parseInt($("#raquette1").css("top"));
-        this.hauteur=parseInt($("#raquette1").css("height"));
-        this.vitesse=1;
-        this.direction=1;
+        /*this.haut=parseInt($("#raquetteG").css("top"));
+        this.hauteur=parseInt($("#raquetteG").css("height"));
+        this.hauteur=parseInt($("#raquetteD").css("height"));
+        this.haut=parseInt($("#raquetteD").css("top"));*/
+        this.haut = parseInt($html.css("top"));
+        this.gauche = parseInt($html.css("left"));
+        this.largeur = $html.width();
+        this.hauteur = $html.height();
+        this.vitesse=1.5;
+        this.direction=0;
+        //this.bas=this.haut+this.hauteur; -> il faut le faire dans une fonction get car il s'agit d'un calcul
+    }
+    // on fait des classes et on déclare les variables dedans pour ensuite les utiliser dans les bouces if
+    
+    //le résultat d'un calcul; get = obtenir et set = définir
+    get bas(){ 
+        return this.haut+this.hauteur;
+    }
 
+    set bas(value) {
+        this.haut = value - this.hauteur;
     }
-    Fraquette1(){ // Créer la fonction "majHTML" dans la class raquette1.
-        this.$html.css("top",raquette1.haut);
+
+    get droite() {
+        return this.gauche+this.largeur;
     }
+
+    set droite(value) {
+        this.largeur = value - this.largeur;
+    }
+    
+    monte(){
+        this.direction=-1;
+    }
+
+    descend(){
+        this.direction=1;
+    }
+
+    stop(){
+        this.direction=0;
+    }
+    
+
+    majHTML(){
+        /*this.$html.css("top",raquetteG.haut);
+        this.$html.css("top",raquetteD.haut);*/
+        this.$html.css("top",this.haut);
+    }  
+
+    bouge(){
+        this.haut=this.haut+this.vitesse*this.direction;
+        this.majHTML(); //importer la valeur dans cette fonction 
+        if (this.haut<0){
+            this.haut=0;
+            this.stop();
+        }
+        if (this.bas>terrain.hauteur){
+            this.bas=terrain.hauteur;
+            this.stop();
+        }
+    }
+
 
 }
 
-class Raquette2{
-    constructor($html){
-        this.$html=$html;
-        this.hauteur=parseInt($("#raquette2").css("height"));
-        this.haut=parseInt($("#raquette2").css("top"));
-        this.vitesse=1;
-        this.direction=1;
-
-
-    }
-    Fraquette2(){
-        this.$html.css("top",raquette2.haut);
-    }
-}
-
-let raquette1 = new Raquette1($("#raquette1")); //Création d'une raquette au sein de la classe "Raquette1"
-let raquette2 = new Raquette2($("#raquette2")); //Création d'une raquette au sein de la classe "raquette2"
+let raquetteG = new Raquette($("#raquetteG"));
+let raquetteD = new Raquette($("#raquetteD"));
