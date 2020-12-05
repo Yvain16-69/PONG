01113dfa7693908e3ad020e62978523614743e01
Yvain Di Costanzo <72392571+Yvain16-69@users.noreply.github.com>
2020-12-05 19:10:48 +0100

    Update Terrain.js

--- js/Terrain.js
+++ js/Terrain.js
@@ -1,9 +1,73 @@
-class Terrain{ //différent de la class css
-    constructor($html){ //constructor permet d'attribuer des valeurs à ce qu'il y a dans les paranthèses
-        this.$html=$html; //$ désigne un élément JQuerie 
-        // $element 
-        this.largeur=$("#terrain").width(); // fait appel à l'élement css width de l'id terrain du css grâce au $
-        this.hauteur=$("#terrain").height(); // on récupère/importe la valeur de la hauteur du terrain depuis le css
+class Terrain {
+    constructor($html) {
+        this.$element = $html;
+        /**
+         * @type {number}
+         */
+        this.largeur = $html.width();
+        /**
+         * @type {number}
+         */
+        this.hauteur = $html.height();
+        /**
+         * @type {JQuery<HTMLElement> | jQuery | HTMLElement}
+         */
+        this.$ecranDebut = $(".ecran-debut");
+    }
+    tiltHaut() {
+        //ajouter une classe
+        this.$element.addClass("tiltHaut");
+        let buffer = this;
+
+        setTimeout(
+            function () {
+                //retirer une classe
+                buffer.$element.removeClass("tiltHaut");
+            }, 200
+        );
+    }
+
+    tiltBas() {
+        //ajouter une classe
+        this.$element.addClass("tiltBas");
+        let buffer = this;
+
+        setTimeout(
+            function () {
+                //retirer une classe
+                buffer.$element.removeClass("tiltBas");
+            }, 200
+        );
+    }
+
+    tiltDroite() {
+        //ajouter une classe
+        this.$element.addClass("tiltDroite");
+        let buffer = this;
+
+        setTimeout(
+            function () {
+                //retirer une classe
+                buffer.$element.removeClass("tiltDroite");
+            }, 200
+        );
+    }
+
+    masqueEcranDebut(){
+        //masque ecran de début
+        this.$ecranDebut.addClass("invisible");
+    }
+
+    tiltGauche() {
+        //ajouter une classe
+        this.$element.addClass("tiltGauche");
+        let buffer = this;
+
+        setTimeout(
+            function () {
+                //retirer une classe
+                buffer.$element.removeClass("tiltGauche");
+            }, 200
+        );
     }
 }
-
